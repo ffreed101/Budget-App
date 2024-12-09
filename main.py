ed5528a2b9970aece25d8fa075fac264b7541a7b
Falon Freed <165129172+ffreed101@users.noreply.github.com>
2024-12-09 09:23:34 -0600

    Created instances of all base functions

---
+++ main.py
@@ -0,0 +1,19 @@
+import json
+
+def main(): # CRUD operation names will go here as my own note
+    pass
+
+def add_transaction(): # Create
+    print("Adds Transaction.")
+
+def view_transactions(): # Read
+    print("Displays all Transaction history")
+
+def view_summary(): # Read
+    print("Displays summary")
+
+def edit_transaction(): # Update
+    print("Edits selected transaction")
+
+def delete_transaction():
+    print("Deletes selected transaction")
