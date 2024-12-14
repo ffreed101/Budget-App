23a6c8f6344bc91f19f399a71fffa7923f59cfb0
Falon Freed <165129172+ffreed101@users.noreply.github.com>
2024-12-14 10:19:02 -0600

    Updated transaction_menu: added comment .gitignore: added .venv main: nothing changed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from scripts.transaction_menu import transaction_menu
+from utils.display_menu import display_menu
 
 if __name__ == "__main__":
     transaction_menu()
--- scripts/transaction_menu.py
+++ scripts/transaction_menu.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir)
+sys.path.append(parent_dir) # Adds parent folder to module import paths
 
 from utils import display_menu
 
