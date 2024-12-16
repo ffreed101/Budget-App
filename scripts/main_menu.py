3266c22bcf789e0bd01a07e91f3f36c5ee3a030f
Falon Freed <165129172+ffreed101@users.noreply.github.com>
2024-12-16 09:34:20 -0600

    main menu created

---
+++ scripts/main_menu.py
@@ -0,0 +1,18 @@
+import sys
+import os
+from datetime import datetime
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir) # Adds parent folder to module import paths
+
+from utils.display_menu import display_menu
+def main_menu():
+    # View Data
+    # Transactions
+    # Profile
+    # Exit
+    display_menu("Main Menu", ("View Data", "Transactions", "Profiles", "Exit"))
+
+
+
