db6a7d297a29d9c687e8d4a0e516eff470e5d61e
Falon Freed <165129172+ffreed101@users.noreply.github.com>
2024-12-18 09:34:29 -0600

    Begin working on profiles Planning on taking a more OOP direction

--- scripts/profile_menu.py
+++ scripts/profile_menu.py
@@ -1,4 +1,5 @@
 from utils.display_menu import display_menu
+from utils.get_valid_input import get_valid_input
 import os.path
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
@@ -6,3 +7,15 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 def profile_menu():
     while True:
         display_menu("Profile Menu", ("Create New Profile", "View Profile", "Edit Profile", "Back"))
+        choice = get_valid_input("Enter a choice", True, False, 1, 4)
+        match choice:
+            case 1:
+                print("Not Implemented")
+            case 2:
+               print("Not Implemented")
+            case 3:
+                print("Not Implememnted")
+            case 4:
+                break
+            case 5:
+                print("Invalid input")
